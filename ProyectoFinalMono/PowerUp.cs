6e36582e5c4bb51bab25664117df6fa30303f22c
Zeus <zeussamaya@gmail.com>
2026-04-27 21:01:32 +0200

    Clase PowerUp, para que este bien se necesita clase Jugador

---
+++ ProyectoFinalMono/PowerUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public abstract class PowerUp
+{
+
+    public string Tipo { get; protected set; }
+    public float Duracion { get; protected set; }
+
+    private float tiempoRestante;
+    private bool activo;
+
+    public PowerUp(string tipo, float duracion)
+    {
+        Tipo = tipo;
+        Duracion = duracion;
+        tiempoRestante = duracion;
+        activo = false;
+    }
+
+    public void Activar(Jugador jugador)
+    {
+        if (!activo)
+        {
+            activo = true;
+            tiempoRestante = Duracion;
+            AplicarEfecto(jugador);
+        }
+    }
+
+    public void Actualizar(float deltaTime, Jugador jugador)
+    {
+        if (activo)
+        {
+            tiempoRestante -= deltaTime;
+
+            if (tiempoRestante <= 0)
+            {
+                Desactivar(jugador);
+                activo = false;
+            }
+        }
+
+
+    }
+
+    public void Desactivar(Jugador jugador)
+    {
+        RemoverEfecto(jugador);
+    }
+
+    protected abstract void AplicarEfecto(Jugador jugador);
+    protected abstract void RemoverEfecto(Jugador jugador);
+}
