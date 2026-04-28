87adc7d43c2079e254a70b655fdd11d2e31630f4
Etienne <etienne.martinez.gonzalez07@gmail.com>
2026-04-28 12:50:42 +0200

    Implementado jugador mapa colisiones y puntos

--- ProyectoFinalMono/Entidadess/Jugador.cs
+++ ProyectoFinalMono/Entidadess/Jugador.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,41 @@ namespace ProyectoFinalMono.Entidadess
 {
     internal class Jugador
     {
+        public Texture2D textura;
+        public Vector2 posicion;
+        public float velocidad = 3f;
+
+        public Jugador(Texture2D tex, Vector2 pos)
+        {
+            textura = tex;
+            posicion = pos;
+        }
+
+        public void Update()
+        {
+            KeyboardState teclado = Keyboard.GetState();
+
+            if (teclado.IsKeyDown(Keys.W))
+                posicion.Y -= velocidad;
+
+            if (teclado.IsKeyDown(Keys.S))
+                posicion.Y += velocidad;
+
+            if (teclado.IsKeyDown(Keys.A))
+                posicion.X -= velocidad;
+
+            if (teclado.IsKeyDown(Keys.D))
+                posicion.X += velocidad;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(textura, posicion, Color.White);
+        }
+
+        public Rectangle Rectangulo()
+        {
+            return new Rectangle((int)posicion.X, (int)posicion.Y, 32, 32);
+        }
     }
 }
